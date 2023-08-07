f57771ab1bc18d9b6296f4fd4aac8af7a056f362
Marius Ellikas <m.ellikas@gmail.com>
2023-08-07 15:33:21 +0300

    commit

--- data_analysis/data_app/views.py
+++ data_analysis/data_app/views.py
@@ -11,20 +11,15 @@ def home(request):
     values = []
 
     for year in years:
-        query = Temperature.objects.filter(
-            Q(year__icontains=year)).aggregate(Avg('avg_temp_C'))
-        if query["avg_temp_C__avg"] is not None:
-            labels.append(year)
-            values.append(round(query["avg_temp_C__avg"], 2))
-        else:
-            labels.append(year)
-            values.append(query["avg_temp_C__avg"])
+        query = Temperature.objects.filter(year=year).aggregate(Avg('avg_temp_C'))
+        labels.append(year)
+        values.append(round(query["avg_temp_C__avg"], 2))
 
     context = {
         "labels": labels,
         "values": values,
-        "city":city,
-        "years":years
+        "city": city,
+        "years": years
     }
     return render(request, template_name='home.html', context=context)
 
@@ -40,9 +35,11 @@ def bar_chart(request):
         query = Temperature.objects.filter(
             Q(month_name__icontains=month) &
             Q(year__icontains=q_year) &
-            Q(city__icontains=q_city)).aggregate(Avg('avg_temp_C'))
-        if query["avg_temp_C__avg"] is not None:
-            values.append(round(query["avg_temp_C__avg"], 2))
+            Q(city__icontains=q_city)).values_list()
+        if query.exists():
+            query_value = query[0]
+            temp = query_value[-1]
+            values.append(temp)
         else:
             values.append(0)
 
@@ -52,10 +49,11 @@ def bar_chart(request):
         'q_axis': q_axis,
         "labels": labels,
         "values": values,
-        "bar_backgroundcolor":bar_backgroundcolor,
-        "bar_bordercolor":bar_bordercolor
+        "bar_backgroundcolor": bar_backgroundcolor,
+        "bar_bordercolor": bar_bordercolor
     }
     return render(request, template_name='bar_chart.html', context=context)
+
 
 def bar_chart_comparison(request):
     q_year_1 = request.GET.get('query_year_1')
@@ -70,36 +68,42 @@ def bar_chart_comparison(request):
     values_1 = []
 
     for month in months:
-        query = Temperature.objects.filter(
+        query_1 = Temperature.objects.filter(
             Q(month_name__icontains=month) &
             Q(year__icontains=q_year_1) &
-            Q(city__icontains=q_city_1)).aggregate(Avg('avg_temp_C'))
-        if query["avg_temp_C__avg"] is not None:
-            values_1.append(round(query["avg_temp_C__avg"], 2))
+            Q(city__icontains=q_city_1)).values_list()
+        if query_1.exists():
+            query_value = query_1[0]
+            temp = query_value[-1]
+            values_1.append(temp)
         else:
             values_1.append(0)
 
     values_2 = []
 
     for month in months:
-        query = Temperature.objects.filter(
+        query_2 = Temperature.objects.filter(
             Q(month_name__icontains=month) &
             Q(year__icontains=q_year_2) &
-            Q(city__icontains=q_city_2)).aggregate(Avg('avg_temp_C'))
-        if query["avg_temp_C__avg"] is not None:
-            values_2.append(round(query["avg_temp_C__avg"], 2))
+            Q(city__icontains=q_city_2)).values_list()
+        if query_2.exists():
+            query_value = query_2[0]
+            temp = query_value[-1]
+            values_2.append(temp)
         else:
             values_2.append(0)
 
     values_3 = []
 
     for month in months:
-        query = Temperature.objects.filter(
+        query_3 = Temperature.objects.filter(
             Q(month_name__icontains=month) &
             Q(year__icontains=q_year_3) &
-            Q(city__icontains=q_city_3)).aggregate(Avg('avg_temp_C'))
-        if query["avg_temp_C__avg"] is not None:
-            values_3.append(round(query["avg_temp_C__avg"], 2))
+            Q(city__icontains=q_city_3)).values_list()
+        if query_3.exists():
+            query_value = query_3[0]
+            temp = query_value[-1]
+            values_3.append(temp)
         else:
             values_3.append(0)
 
@@ -120,10 +124,10 @@ def bar_chart_comparison(request):
     }
     return render(request, template_name='bar_chart_comparison.html', context=context)
 
+
 def polar_area(request):
     q_year = request.GET.get('query_year')
     q_city = request.GET.get('query_city')
-    q_axis = request.GET.get('query_axis')
     labels = months
     values = []
 
@@ -131,16 +135,17 @@ def polar_area(request):
         query = Temperature.objects.filter(
             Q(month_name__icontains=month) &
             Q(year__icontains=q_year) &
-            Q(city__icontains=q_city)).aggregate(Avg('avg_temp_C'))
-        if query["avg_temp_C__avg"] is not None:
-            values.append(round(query["avg_temp_C__avg"], 2))
+            Q(city__icontains=q_city)).values_list()
+        if query.exists():
+            query_value = query[0]
+            temp = query_value[-1]
+            values.append(temp)
         else:
             values.append(0)
 
     context = {
         'q_year': q_year,
         'q_city': q_city,
-        'q_axis': q_axis,
         "labels": labels,
         "values": values,
     }
